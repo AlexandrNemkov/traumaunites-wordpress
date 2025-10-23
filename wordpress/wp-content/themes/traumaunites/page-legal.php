9cf46c93f5b069613e876f36c7f608da8344901a
alexandnemkov <i7778aj@gmail.com>
2025-10-23 12:54:30 +0300

    Add legal page with privacy policy and cookies policy

---
+++ wordpress/wp-content/themes/traumaunites/page-legal.php
@@ -0,0 +1,66 @@
+<?php
+/*
+Template Name: Legal Page
+*/
+get_header(); ?>
+
+<section class="relative mt-24">
+    <div class="container">
+        <div class="flex items-center gap-4 flex-wrap text-black text-opacity-80">
+            <a href="<?php echo home_url(); ?>" class="transition hover:text-blue-60">Main</a>
+            <svg class="size-16"><use href="<?php echo get_template_directory_uri(); ?>/assets/img/sprite.svg#right"></use></svg>
+            <span class="text-opacity-100 text-blue-60">Legal notice</span>
+        </div>
+    </div>
+</section>
+
+<section class="relative mt-40 lg:mt-64">
+    <div class="container">
+        <article class="max-w-800 mx-auto">
+            <h1 class="text-28 font-semibold leading-none text-center lg:text-48">Legal notice</h1>
+            <div class="tw-html tw-html--page mt-40">
+                <h2>Data protection policy</h2>
+                <p>- Unitradent Medical SL informs you about its personal data protection policy.</p>
+                <p>- Unitradent Medical SL reserves the right to modify this policy, informing you in advance of any changes that may occur through this notice.</p>
+                <p>- You, by voluntarily providing your personal data through any form on this website or email message and accepting its sending, expressly authorize Unitradent Medical SL to process and incorporate the data sent into a file owned by you.</p>
+                <p>- The collection and processing of the personal data that you provide is intended to contact you to respond to your request and inform you about the products and services offered by this company, through any means of communication, including electronic.</p>
+                <p>- The personal data that you provide must be true, accurate, complete and updated, being responsible for any damages that may be caused due to non-compliance with this obligation.</p>
+                <p>- Unitradent Medical SL has adopted the necessary technical measures to avoid the alteration, loss or unauthorized processing of the personal data that you provide, taking into account the limitations of the Internet.</p>
+                <p>- Unitradent Medical SL expresses its commitment to confidentiality regarding the processing of your personal data.</p>
+                <h2>Cookies policy</h2>
+                <p>We use our own and third-party cookies to evaluate the use of the Website and its general activity. If you continue navigating, we consider you accept its use.</p>
+                <p>A cookie is a file that is downloaded to your computer when you access certain web pages. Cookies allow a website, among other things, to store and retrieve information about the browsing habits of a user or their equipment and, depending on the information they contain and the way in which their equipment is used, they can be used to recognize it.</p>
+                <p>However, these media only obtain information related to the number of pages visited, the city to which the IP address from which it is accessed is assigned, the number of new users, the frequency and recurrence of visits, the visit time. , the browser or the operator or type of terminal from which the visit is made. In no case is data obtained about the name, surname or postal address from which the user has connected.</p>
+                <p>The cookies used on this website and the specific purpose of each of them are the following:</p>
+                <p>PHPSESSID Cookie: This cookie is used to establish the session of the user who visits one of our Websites. It is a way to identify and manage the state - the session variables - for a specific user, and to be able to move that information through the pages of our Website.</p>
+                <p>UTMA Cookie: We use this cookie to count how many times a unique user visits the site.</p>
+                <p>UTMB Cookie: We use this cookie to calculate the time a user remains on a page.</p>
+                <p>UTMZ Cookie: This cookie stores the origin of the visitor, the path followed to access the website, whether direct access, from a link on another website, from an email link, using certain keywords in a search engine. , through a display campaign, or through an AdWords ad.</p>
+                <p>UTMC Cookie: The current javascript code used by Google Analytics does not require this cookie. Previously, this cookie was used together with the _utmb cookie to determine whether or not a new session should be established for the user after more than 30 minutes on the same page. This cookie continues to be written to ensure compatibility with websites where the old urchin.js tracking code is installed.</p>
+                <p>CPV Cookie:</p>
+                <p>CT_IS Cookie:</p>
+                <p>IS</p>
+                <p>Cookie: PVC</p>
+                <p>Cookie: QUISMA_T_UK</p>
+                <p>Cookie: QUISMA_T_USER Cookie:</p>
+                <p>SESSION_SESSION Cookie</p>
+                <p>: SH Cookie:</p>
+                <p>TUUID Cookie:</p>
+                <p>USER_DATA_SYNCED Cookie:</p>
+                <p>You can allow, block or delete cookies installed on your computer by configuring your browser options. You can find information on how to do this, in relation to the most common browsers, in the links included below:</p>
+                <p>Explorer:</p>
+                <p>http://windows.microsoft.com/es-es/windows7/how-to-manage-cookies-in-internet-explorer-9</p>
+                <p>Firefox:</p>
+                <p>https://support.mozilla.org/es/kb/cookies-informacion-que-los-sitios-web-guardan-en-</p>
+                <p>Chrome:</p>
+                <p>http://support.google.com/chrome/bin/answer.py?hl=es&answer=95647</p>
+                <p>Safari:</p>
+                <p>http://support.apple.com/kb/ph5042</p>
+                <p>&nbsp;</p>
+                <p>We inform you, however, of the possibility that deactivating any cookie prevents or hinders navigation or the provision of the services offered on this Website.</p>
+            </div>
+        </article>
+    </div>
+</section>
+
+<?php get_footer(); ?>
